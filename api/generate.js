8beb9f836ad104a6e565d3a495c4d1c34fcf7151
Maizil <142029402+Maizil41@users.noreply.github.com>
2025-06-02 14:01:24 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,17 +1,22 @@
-const QRCode = require('qrcode');
+import QRCode from 'qrcode';
 
 export default async function handler(req, res) {
-  const { text } = req.query;
+  const { text, format } = req.query;
 
   if (!text) {
     return res.status(400).json({ error: 'Parameter "text" dibutuhkan' });
   }
 
   try {
-    const qrDataURL = await QRCode.toDataURL(text);
-    res.status(200).json({ text, qrcode: qrDataURL });
+    if (format === 'png') {
+      const buffer = await QRCode.toBuffer(text);
+      res.setHeader('Content-Type', 'image/png');
+      res.send(buffer);
+    } else {
+      const dataUrl = await QRCode.toDataURL(text);
+      res.status(200).json({ text, qrcode: dataUrl });
+    }
   } catch (err) {
     res.status(500).json({ error: 'Gagal generate QR Code' });
   }
 }
-
